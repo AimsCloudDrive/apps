246c0c91f1b826ec91c12eb10432de4db1c18e11
AimsCloudDrive <i79401045@126.com>
2024-06-02 10:37:15 +0800

    #0: misc: todo: update

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
   ],
   npmClient: 'pnpm',
 });
